e47ae4e1046ec17496b5f6f6368d52bf0d751920
DEMORGNY Gilbert <gdemorgny@dreamtogo.fr>
2023-01-10 10:42:01 +0100

    add event

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -1,7 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Board : MonoBehaviour
 {
+    //Unity Event on déclare l'évent
+    public UnityEvent OnWin;
+
+    //Version C# on relie l'appel du delegate à l'event. 
+    // La définition du delegate va déterminer quelles informations seront disponibles. 
+    // ATTENTION la signature du delegate doit être respectée par les fonctions qui écoutent.
+    public delegate void MessageEvent();
+    public static event MessageEvent OnLoose;
+
+    public void CheckActualRow()
+    {
+        Debug.Log("J'ai appuyé sur le bouton !");
+        // Unity Event : invocation de l'évent, le ? vérifie si l'évent n'est pas déjà en cours.
+        //OnWin?.Invoke();
+
+        //C# : on invoke l'event. Cette synthaxe correspond à la synthaxe avec le ?. Cela revient au même, vous risquez de trouver les deux
+        if(OnLoose != null)
+        {
+            OnLoose.Invoke();
+        }
+    }
 }
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -4,4 +4,35 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
+    [SerializeField] private GameObject _winMessage;
+    [SerializeField] private GameObject _looseMessage;
+    //Variable qui permettra d'acceder à l'event version Unity
+    [SerializeField] private Board _board;
+
+    private void OnEnable()
+    {
+        //UnityEvent : On inscrit la fonction WinMessage à l'event OnWin.
+        //On a absolument besoin de l'acces à l'objet _board instancié à partir de la classe Board
+        _board.OnWin.AddListener(WinMessage);
+
+        //C# : on inscrit la fonction LooseMessage à l'event OnLoose.
+        //grace à la propriété static nous devons juste trouver la classe Board
+        Board.OnLoose += LooseMessage;
+    }
+    private void OnDisable()
+    {
+        // dans les deux cas on se déinscrit de l'event
+        _board.OnWin.RemoveListener(WinMessage);
+        Board.OnLoose -= LooseMessage;
+
+    }
+    private void WinMessage()
+    {
+        _winMessage.SetActive(true);
+    }
+    private void LooseMessage()
+    {
+        _looseMessage.SetActive(true);
+
+    }
 }
